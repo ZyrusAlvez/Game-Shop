5ec9967bdc1cdf0916cd4f7762ffc38e94352f3f
ZyrusAlvez <zyrusalvez13@gmail.com>
2025-12-13 22:19:03 +0800

    test: simulate product data

---
+++ info.ts
@@ -0,0 +1,36 @@
+export const shop = [
+    {
+        gameName: "Roblox",
+        ImagePath: "/Roblox.png",
+        games : [
+            {
+                gameName: "The Forge",
+                ImagePath: "/Roblox/The Forge.jpg",
+                products: [
+                    {
+                        id: "roblox-forge-1",
+                        name: "Flare Blood Kamas",
+                        price: 3.99,
+                        imagePath: "/Roblox/The Forge/roblox-forge-1.jpg",
+                    }
+                ]
+            },
+            {
+                gameName: "Creature of Sonaria",
+                ImagePath: "/Roblox/Creature of Sonaria.jpg"
+            }
+        ]
+    },
+    {
+        gameName: "Valorant",
+        imagePath: "/Valorant.jpg",
+        products: [
+                    {
+                        id: "roblox-forge-1",
+                        name: "Flare Blood Kamas",
+                        price: 3.99,
+                        imagePath: "/Roblox/The Forge/roblox-forge-1.jpg",
+                    }
+                ]
+    }
+]
